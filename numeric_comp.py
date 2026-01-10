896ca6691a2a41a25fbac9b461093150a6eeb8bd
Odhiambo <geoffrey.ochieng.odhiambo03@gmail.com>
2026-01-10 15:29:18 +0300

    Numpy arrays

--- numeric_comp.py
+++ numeric_comp.py
@@ -1,11 +1,12 @@
+import numpy as np
 
-weights = [0.3, 0.2, 0.5]
+weights = np.array([0.3, 0.2, 0.5])
 
-kanto = [73, 67, 43]
-johto = [91, 88, 64]
-hoenn = [87, 134, 58]
-sinnoh = [102, 43, 37]
-unova = [69, 96, 70]
+kanto = np.array([73, 67, 43])
+johto = np.array([91, 88, 64])
+hoenn = np.array([87, 134, 58])
+sinnoh = np.array([102, 43, 37])
+unova = np.array([69, 96, 70])
 
 def crop_yield(region, weights):
     result = 0
@@ -13,6 +14,13 @@ def crop_yield(region, weights):
         result += r * w
     return result
 
-print(f'Crop yield for Kanto: {crop_yield(kanto, weights)}')
-print(f'Crop yield for Johto: {crop_yield(johto, weights)}')
-print(f'Crop yield for Unova: {crop_yield(unova, weights)}')
+# print(f'Crop yield for Kanto: {crop_yield(kanto, weights)}')
+# print(f'Crop yield for Johto: {crop_yield(johto, weights)}')
+# print(f'Crop yield for Unova: {crop_yield(unova, weights)}')
+
+print(type(kanto))
+print(type(weights))
+
+# Indexing in Numpy
+print(weights[0])
+print(unova[2])
