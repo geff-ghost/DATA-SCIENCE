b7bcac7aff8b7c0f2874c5a3e0b551760eda24b2
Odhiambo <geoffrey.ochieng.odhiambo03@gmail.com>
2026-01-10 15:17:40 +0300

    Python Lists

--- numeric_comp.py
+++ numeric_comp.py
@@ -1,10 +1,18 @@
-import numpy as np
 
-arr1 = np.array([
-    [1, 2, 3, 4],
-    [9, 8, 7, 6]
-])
+weights = [0.3, 0.2, 0.5]
 
-print(arr1)
-print(arr1.shape)
-print(type(arr1))
+kanto = [73, 67, 43]
+johto = [91, 88, 64]
+hoenn = [87, 134, 58]
+sinnoh = [102, 43, 37]
+unova = [69, 96, 70]
+
+def crop_yield(region, weights):
+    result = 0
+    for r, w in zip(region, weights):
+        result += r * w
+    return result
+
+print(f'Crop yield for Kanto: {crop_yield(kanto, weights)}')
+print(f'Crop yield for Johto: {crop_yield(johto, weights)}')
+print(f'Crop yield for Unova: {crop_yield(unova, weights)}')
