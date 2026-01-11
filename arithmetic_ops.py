351a09246b79c51ed1ce1b94c9146f3095f4e5ec
Odhiambo <geoffrey.ochieng.odhiambo03@gmail.com>
2026-01-11 19:05:11 +0300

    Logical Operations

--- arithmetic_ops.py
+++ arithmetic_ops.py
@@ -14,22 +14,35 @@ arr3 = np.array([
 
 print(arr2 + arr3)
 
-# Adding a scalar
-print(arr2 + 3)
-print()
-# Element-wise subtraction
-print(arr3 - arr2)
-print()
-# Division by scalar
-print(arr2 / 2)
-print()
-# Element-wise multiplication
-print(arr2 * arr3)
-print()
-# Modulus with scalar
-print(arr2 % 4)
+# # Adding a scalar
+# print(arr2 + 3)
+# print()
+# # Element-wise subtraction
+# print(arr3 - arr2)
+# print()
+# # Division by scalar
+# print(arr2 / 2)
+# print()
+# # Element-wise multiplication
+# print(arr2 * arr3)
+# print()
+# # Modulus with scalar
+# print(arr2 % 4)
 
-arr5 = np.array([2, 3])
-print(arr5.shape)
+# arr5 = np.array([2, 3])
+# print(arr5.shape)
 
+print('---Logical Operations---')
+arr1 = np.array([[1, 2, 3], [3, 4, 5]])
+arr2 = np.array([[2, 2, 3], [1, 2, 3]])
+
+print(f"arr1:\n{arr1}")
+print()
+print(f"arr2:\n{arr2}")
+
+print(f'\n arr1 == arr2 \n{arr1 == arr2}')
+print(f'\n arr1 != arr2 \n{arr1 != arr2}')
+print(f'\n arr1 >= arr2 \n{arr1 >= arr2}')
+print(f'\n arr1 < arr2 \n{arr1 < arr2}')
+print(f'\n(arr1 == arr2).sum():{(arr1 == arr2).sum()}')
 
