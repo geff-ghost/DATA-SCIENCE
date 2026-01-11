fc209aaa7a290cee8a8c9b413d4f5057848b19a7
Odhiambo <geoffrey.ochieng.odhiambo03@gmail.com>
2026-01-11 19:24:30 +0300

    Array indexing and slicing

--- arithmetic_ops.py
+++ arithmetic_ops.py
@@ -44,5 +44,46 @@ print(f'\n arr1 == arr2 \n{arr1 == arr2}')
 print(f'\n arr1 != arr2 \n{arr1 != arr2}')
 print(f'\n arr1 >= arr2 \n{arr1 >= arr2}')
 print(f'\n arr1 < arr2 \n{arr1 < arr2}')
-print(f'\n(arr1 == arr2).sum():{(arr1 == arr2).sum()}')
+print(f'\n(arr1 == arr2).sum(): {(arr1 == arr2).sum()}')
 
+
+print('\n\n---Array indexing and slicing---\n')
+arr3 = np.array([
+    [[11, 12, 13, 14],
+    [13, 14, 15, 19]],
+
+    [[15, 16, 17, 21],
+    [63, 92, 36, 18]],
+
+    [[98, 32, 81, 23],
+    [17, 18, 19.5, 43]]
+])
+print(f"arr3:\n{arr3}")
+print(arr3.shape)
+print()
+
+# Single Element
+print(arr3[1, 1, 2])
+print()
+
+# Subarray using ranges
+print(arr3[1:, :1, :2])
+print(arr3[1:, :1, :2].shape)
+print()
+
+# Mixing indices and ranges
+print(arr3[1:, 1, 3])
+print(arr3[1:, 1, 3].shape)
+print()
+
+print(arr3[:, 1, :3])
+print(arr3[:, 1, :3].shape)
+print()
+
+# Using fewer indices
+print(arr3[:2, 1])
+print(arr3[:2, 1].shape)
+print()
+
+# Using too many indices
+# print(arr3[1, 3, 2, 1]) -> IndexOutOfRange
