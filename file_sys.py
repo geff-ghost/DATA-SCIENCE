d5b944f325e6c912562599aafac0363931169768
Odhiambo <geoffrey.ochieng.odhiambo03@gmail.com>
2026-01-14 18:06:45 +0300

    Creating and Writing to files

--- file_sys.py
+++ file_sys.py
@@ -1,3 +1,5 @@
+import os
+from functions import loan_emi
 
 with open('./data/loans1.txt', 'r') as file1:
     file1_content = file1.read()
@@ -49,12 +51,56 @@ def read_csv(path):
             result.append(item_dict)
     return result
 
-print(read_csv('./data/loans1.txt'))
-print()
-print(read_csv('./data/loans2.txt'))
-print()
-print(read_csv('./data/loans3.txt'))
-print()
-print(read_csv('./data/students.txt'))
+def compute_emis(loans):
+    for loan in loans:
+        loan['emi'] = loan_emi(
+            amount=loan['amount'],
+            rate=loan['rate'] / 12, # the CSV contains yearly rates
+            duration=loan['duration'],
+            down_payment=loan['down_payment']
+        )
 
 
+
+def write_csv(items, path):
+    # Open the file in write mode
+    with open(path, 'w') as f:
+        # Return if there's nothing to write
+        if len(items) == 0:
+            return
+        
+        # Write the headers in the first line
+        headers = list(items[0].keys())
+        f.write(','.join(headers) + '\n')
+
+        # Write one item per line
+        for item in items:
+            values = []
+            for header in headers:
+                values.append((str(item.get(header, ''))))
+            f.write(','.join(values) + '\n')
+
+loans3 = read_csv('./data/loans3.txt')
+compute_emis(loans3)
+write_csv(loans3, './data/emis3.txt')
+
+with open('./data/emis3.txt', 'r') as f:
+    print(f.read())
+
+print()
+
+loans1 = read_csv('./data/loans1.txt')
+compute_emis(loans1)
+write_csv(loans1, './data/emis1.txt')
+
+with open('./data/emis1.txt', 'r') as f:
+    print(f.read())
+
+print()
+
+loans2 = read_csv('./data/loans2.txt')
+compute_emis(loans2)
+write_csv(loans2, './data/emis2.txt')
+
+with open('./data/emis2.txt', 'r') as f:
+    print(f.read())
--- functions.py
+++ functions.py
@@ -28,16 +28,16 @@ def loan_emi(amount, duration, rate, down_payment=0):
     emi = math.ceil(emi)
     return emi
 
-emi1 = loan_emi(amount=1.26e6, duration=8*12, rate=0.1/12, down_payment=3e5)
-emi2 = loan_emi(amount=1.26e6, duration=10*12, rate=0.08/12)
+# emi1 = loan_emi(amount=1.26e6, duration=8*12, rate=0.1/12, down_payment=3e5)
+# emi2 = loan_emi(amount=1.26e6, duration=10*12, rate=0.08/12)
 
-print(f'emi1: {emi1}')
-print(f'emi2: {emi2}')
+# print(f'emi1: {emi1}')
+# print(f'emi2: {emi2}')
 
-if emi1 < emi2:
-    print(f'Option1 has a lower EMI: ${emi1}')
-else:
-    print(f'Option2 has a lower EMI: ${emi2}')
+# if emi1 < emi2:
+#     print(f'Option1 has a lower EMI: ${emi1}')
+# else:
+#     print(f'Option2 has a lower EMI: ${emi2}')
 
 
 """Shaun is currently paying back a home loan for a house he took
@@ -53,27 +53,27 @@ house_down_payment = .25 * cost_of_house
 house_loan_duration = 6*12 # months
 house_loan_rate = 0.07/12 # monthly
 
-emi_house = loan_emi(
-    amount= cost_of_house,
-    duration=house_loan_duration,
-    rate=house_loan_rate,
-    down_payment=house_down_payment
-)
-print(f'House EMI: ${emi_house}')
+# emi_house = loan_emi(
+#     amount= cost_of_house,
+#     duration=house_loan_duration,
+#     rate=house_loan_rate,
+#     down_payment=house_down_payment
+# )
+# print(f'House EMI: ${emi_house}')
 
 cost_of_car = 60000
 car_loan_duration = 1*12 # months
 car_loan_rate = .12/12 # monthly
 
-emi_car = loan_emi(amount=cost_of_car, duration=car_loan_duration, rate=car_loan_rate)
-print(f'Car EMI: ${emi_car}')
+# emi_car = loan_emi(amount=cost_of_car, duration=car_loan_duration, rate=car_loan_rate)
+# print(f'Car EMI: ${emi_car}')
 
-print(f'\nShaun makes a total monthly payment of ${emi_car + emi_house} per month.')
+# print(f'\nShaun makes a total monthly payment of ${emi_car + emi_house} per month.')
 
 
 """If you borrow $100,000 using a 10-year loan with an interest rate of 9%
 per annum, what is the total amount you end up paying as interest?"""
 
-emi_with_interest = loan_emi(amount=100000, duration=10*12, rate=0.09/12)
-emi_without_interest = loan_emi(amount=100000, duration=10*12, rate=0./12)
-print('Loan with interest: ${}\nLoan without interest: ${}'.format(emi_with_interest, emi_without_interest))
+# emi_with_interest = loan_emi(amount=100000, duration=10*12, rate=0.09/12)
+# emi_without_interest = loan_emi(amount=100000, duration=10*12, rate=0./12)
+# print('Loan with interest: ${}\nLoan without interest: ${}'.format(emi_with_interest, emi_without_interest))
