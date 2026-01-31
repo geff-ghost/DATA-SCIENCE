3672354afc9be85c48a332f81a0ad5a4e0d1af42
Odhiambo <geoffrey.ochieng.odhiambo03@gmail.com>
2026-01-31 16:38:17 +0300

    Retrieve data from a data frame

--- covid_df.py
+++ covid_df.py
@@ -2,20 +2,47 @@ import pandas as pd
 
 covid_df = pd.read_csv('italy-covid-daywise.csv')
 
-# Basic information about the data frame
-print(covid_df.info())
+# # Basic information about the data frame
+# print(covid_df.info())
 
-print()
+# print()
 
-# Statistical information
-print(covid_df.describe())
+# # Statistical information
+# print(covid_df.describe())
 
-print()
+# print()
 
-# Columns in a data frame
-print(covid_df.columns)
+# # Columns in a data frame
+# print(covid_df.columns)
 
-print()
+# print()
 
-# Number of columns and rows
-print(covid_df.shape)
+# # Number of columns and rows
+# print(covid_df.shape)
+
+# Pandas format is similar to this
+covid_data_dict = {
+    'date': ['2020-08-30', '2020-08-31', '2020-09-01', '2020-09-02', '2020-09-03'],
+    'new_cases': [144, 1365, 996, 95, 1326],
+    'new_deaths': [1, 4, 6, 8, 6],
+    'new_test': [53541, 42583, 54395, None, None]
+}
+
+print(covid_data_dict['new_cases'])
+
+print(covid_df['new_cases'][23])
+
+print(covid_df['new_deaths'][45])
+
+print(covid_df.at[34, 'new_tests'])
+
+cases_df = covid_df[['date', 'new_cases']]
+# print(cases_df)
+
+covid_df_copy = covid_df.copy()
+
+print(covid_df.loc[43])
+
+print(covid_df.new_tests.first_valid_index())
+
+print(covid_df.loc[13 : 16])
